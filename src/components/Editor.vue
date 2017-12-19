04af9d5e58e41984678a86c4d5f21335c928f51c
sk <949919743@qq.com>
2017-12-19 20:08:59 +0800

    editor=>Tab

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -2,34 +2,34 @@
     <div id="editor">
         <nav>
             <ol>
-                <li class="active">
+                <li v-bind:class="{active: currentTab === 0}" v-on:click="currentTab = 0">
                     <svg class="icon">
                         <use xlink:href="#icon-shenfenzheng"></use>
                     </svg>
                 </li>
-                <li>
+                <li v-bind:class="{active: currentTab === 1}" v-on:click="currentTab = 1">
                     <svg class="icon">
-                        <use xlink:href="#icon-work"></use>
+                        <use xlink:href="#icon-book1"></use>
                     </svg>
                 </li>
-                <li>
+                <li v-bind:class="{active: currentTab === 2}" v-on:click="currentTab = 2">
                     <svg class="icon">
-                        <use xlink:href="#icon-project"></use>
+                        <use xlink:href="#icon-jiangbei1"></use>
                     </svg>
                 </li>
-                <li>
+                <li v-bind:class="{active: currentTab === 3}" v-on:click="currentTab = 3">
                     <svg class="icon">
-                        <use xlink:href="#icon-book"></use>
+                        <use xlink:href="#icon-gongzuoxinxi"></use>
                     </svg>
                 </li>
-                <li>
+                <li v-bind:class="{active: currentTab === 4}" v-on:click="currentTab = 4">
                     <svg class="icon">
-                        <use xlink:href="#icon-jiangbei"></use>
+                        <use xlink:href="#icon-xiangmu1"></use>
                     </svg>
                 </li>
-                <li>
+                <li v-bind:class="{active: currentTab === 5}" v-on:click="currentTab = 5">
                     <svg class="icon">
-                        <use xlink:href="#icon-phone"></use>
+                        <use xlink:href="#icon-phone1"></use>
                     </svg>
                 </li>
             </ol>
@@ -40,29 +40,39 @@
     </div>
 </template>
 
+<script>
+    export default {
+        data(){
+            return {
+                currentTab: 0
+            }
+        }
+    }
+</script>
+
 <style lang="scss">
     #editor{
         min-height: 200px;
         display: flex;
+
         > nav{
             background: #000;
             width: 80px;
 
             > ol > li{
-                border: 1px solid red;
-                padding: 8px 0;
+                padding: 16px 0;
                 text-align: center;
 
                 > .icon {
                     width: 24px;
                     height: 24px;
-                    fill: #fff;
+                    fill: white;
                 }
 
                 &.active{
-                    background: #fff;
+                    background: white;
                     > .icon{
-                        fill: #000;
+                        fill: black;
                     }
                 }
             }
